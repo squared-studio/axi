067f4651777806c77192c396ebbb696d5c11ebc5
Foez Ahmed <foez.official@gmail.com>
2024-10-11 12:53:13 +0600

    modified:   rtl/axi_grid_xni.sv

--- rtl/axi_grid_xni.sv
+++ rtl/axi_grid_xni.sv
@@ -7,7 +7,7 @@ Author : Foez Ahmed (foez.official@gmail.com)
 `include "axi_default_param_pkg.sv"
 
 module axi_grid_xni #(
-    parameter bit       IS_PIPELINED   = 0,
+    parameter bit       IS_PIPELINED   = 1,
     parameter type      grid_id_t      = axi_default_param_pkg::grid_id_t,
     parameter type      grid_aw_chan_t = axi_default_param_pkg::grid_aw_chan_t,
     parameter type      grid_w_chan_t  = axi_default_param_pkg::grid_w_chan_t,
@@ -23,33 +23,47 @@ module axi_grid_xni #(
 
   `AXI_GRID_VH_JOIN_SPLIT_INST_FULL
 
+  `define AXI_GRID_XNI_PIPELINE(__CHAN__)                                                         \
+    pipeline #(                                                                                   \
+        .ELEM_WIDTH(                                                                              \
+            $bits(``__CHAN__``did_o)                                                              \
+          + $bits(``__CHAN__``sid_o)                                                              \
+          + $bits(``__CHAN__``chan_o))                                                            \
+    ) u_pipeline_``__CHAN__`` (                                                                   \
+        .arst_ni(arst_ni),                                                                        \
+        .clk_i(clk_i),                                                                            \
+        .rst_i('1),                                                                               \
+        .elem_in_i({``__CHAN__``did_i, ``__CHAN__``sid_i, ``__CHAN__``chan_i}),                   \
+        .elem_in_valid_i(``__CHAN__``valid_i),                                                    \
+        .elem_in_ready_o(``__CHAN__``ready_o),                                                    \
+        .elem_out_o({``__CHAN__``did_o, ``__CHAN__``sid_o, ``__CHAN__``chan_o}),                  \
+        .elem_out_valid_o(``__CHAN__``valid_o),                                                   \
+        .elem_out_ready_i(``__CHAN__``ready_i)                                                    \
+    );                                                                                            \
+
+
+  `define AXI_GRID_O_I(__SIG__) assign ``__SIG__``_o = ``__SIG__``_i;
+
+  `define AXI_GRID_O_I_CHAN(__CHAN__)                                                             \
+    `AXI_GRID_O_I(``__CHAN__``did)                                                                \
+    `AXI_GRID_O_I(``__CHAN__``sid)                                                                \
+    `AXI_GRID_O_I(``__CHAN__``chan)                                                               \
+    `AXI_GRID_O_I(``__CHAN__``valid)                                                              \
+    `AXI_GRID_O_I(``__CHAN__``ready)                                                              \
+
+
   if (IS_PIPELINED) begin : gen_pipeline
+    `AXI_GRID_XNI_PIPELINE(aw)
+    `AXI_GRID_XNI_PIPELINE(w)
+    `AXI_GRID_XNI_PIPELINE(b)
+    `AXI_GRID_XNI_PIPELINE(ar)
+    `AXI_GRID_XNI_PIPELINE(r)
   end else begin : gen_direct
-    assign awdid_o   = awdid_i;
-    assign awsid_o   = awsid_i;
-    assign awchan_o  = awchan_i;
-    assign awvalid_o = awvalid_i;
-    assign awready_o = awready_i;
-    assign wdid_o    = wdid_i;
-    assign wsid_o    = wsid_i;
-    assign wchan_o   = wchan_i;
-    assign wvalid_o  = wvalid_i;
-    assign wready_o  = wready_i;
-    assign bdid_o    = bdid_i;
-    assign bsid_o    = bsid_i;
-    assign bchan_o   = bchan_i;
-    assign bvalid_o  = bvalid_i;
-    assign bready_o  = bready_i;
-    assign ardid_o   = ardid_i;
-    assign arsid_o   = arsid_i;
-    assign archan_o  = archan_i;
-    assign arvalid_o = arvalid_i;
-    assign arready_o = arready_i;
-    assign rdid_o    = rdid_i;
-    assign rsid_o    = rsid_i;
-    assign rchan_o   = rchan_i;
-    assign rvalid_o  = rvalid_i;
-    assign rready_o  = rready_i;
+    `AXI_GRID_O_I_CHAN(aw)
+    `AXI_GRID_O_I_CHAN(w)
+    `AXI_GRID_O_I_CHAN(b)
+    `AXI_GRID_O_I_CHAN(ar)
+    `AXI_GRID_O_I_CHAN(r)
   end
 
 endmodule
